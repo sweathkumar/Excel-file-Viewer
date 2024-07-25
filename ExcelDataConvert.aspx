2c2ad375e87f5cda995908c620cf14138462b3e9
Sweathkumar <121376815+sweathkumar@users.noreply.github.com>
2024-07-25 17:26:31 +0530

    Add files via upload

---
+++ ExcelDataConvert.aspx
@@ -0,0 +1,116 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="ExcelDataConvert.aspx.cs" Inherits="ExcelFileToTableConvert.ExcelDataConvert" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>ExcelFileToTableConvert</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous" />
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <!-- Include jQuery -->
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <!-- Include Footable CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/footable/css/footable.core.min.css" rel="stylesheet">
+    <!-- Include Footable JavaScript -->
+    <script src="https://cdn.jsdelivr.net/npm/footable/dist/footable.all.min.js"></script>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <%--Section one header--%>
+    <section class="py-3 header bg-dark">
+        <div class="container text-white">
+            <div class="row">
+                <p class="text-center fs-4 fw-bold">Excel File To Table Convert</p>
+            </div>
+        </div>
+    </section>
+        <%--section two content--%>
+    <section class="py-5 body">
+        <div class="container">
+            <div class="row fs-5 mx-5 text-dark justify-content-center border border-1 border-dark p-5">
+                <div class="col-3 d-flex justify-content-center align-items-center">
+                    <%--text--%>
+                    <p class="fs-5 fw-normal text-dark m-0">Upload Excel File</p>
+                </div>
+                <div class="col-5 d-flex justify-content-center align-items-center">
+                    <%--file uploder--%>
+                    <asp:FileUpload ID="uploadFile" runat="server"/>
+                </div>
+                <div class="col-1 d-flex justify-content-center align-items-center">
+                    <%--upload button--%>
+                    <asp:Button CssClass="btn btn-primary" ID="btnUpload" runat="server" Text="Upload" OnClick="btnUpload_Click"/>
+                </div>
+                <div class="col-1 d-flex justify-content-center align-items-center">
+                    <%--view button--%>
+                    <asp:Button CssClass="btn btn-outline-success" ID="btnView" runat="server" Text="View" OnClick="btnView_Click" />
+                </div>
+            </div>
+            <div class="row mx-5 justify-content-end">
+                <%--note msg--%>
+                <p class="fs-6 fw-light text-end">*This page is under Development Try Uploading Only Excel Files</p>
+            </div>
+        </div>
+    </section>
+        <%--section two grid--%>
+    <section class="py-2 pb-5 body-2">
+        <div class="container mb-3">
+            <%--repeater--%>
+            <asp:Repeater ID="repeaterView" runat="server">
+                <%--header--%>
+                <HeaderTemplate>
+                    <%--table--%> 
+                <table id="tableView" border="1" class="table table-dark table-striped">
+                    <thead>
+                         <tr>
+                            <th>Serial No</th>
+                            <th>User ID</th>
+                            <th>Name</th>
+                            <th data-breakpoints="xs sm">Email</th>
+                            <th data-breakpoints="xs sm">Phone number</th>
+                         </tr>
+                        </thead>
+                    <tbody>
+                     </HeaderTemplate>
+                <%--Table Column--%> 
+                     <ItemTemplate>
+                         <tr>
+                            <td><%# Eval("srno") %></td>
+                            <td><%# Eval("userid") %></td>
+                            <td><%# Eval("name") %></td>
+                            <td><%# Eval("email") %></td>
+                            <td><%# Eval("phone") %></td>
+                         </tr>
+                     </ItemTemplate>
+                <%--footer template--%> 
+                    <FooterTemplate>
+                        </tbody>
+                        <tfoot>
+                            <tr>
+                                <td colspan="4">
+                                    <div class="pagination pagination-centered d-flex"></div>
+                                </td>
+                            </tr>
+                        </tfoot>
+                        </table>
+                    </FooterTemplate>
+            </asp:Repeater>
+        </div>
+    </section>
+        <%--section for footer--%>
+    <section class="py-2 bg-dark footer fixed-bottom">
+        <div class="container text-white">
+            <div class="row">
+                <p class="text-center fs-5 fw-normal">Sweathkumar Project 2024</p>
+            </div>
+        </div>
+    </section>
+    </form>
+    <%--script--%> 
+    <script>
+        <%--footables
+        $(document).ready(function () {
+            $('#tableView').footable();
+        });--%>
+    </script>
+</body>
+</html>
---
+++ ExcelDataConvert.aspx.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.OleDb;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace ExcelFileToTableConvert
+{
+    public partial class ExcelDataConvert : System.Web.UI.Page
+    {
+        // connection
+        OleDbConnection con;
+        string constring;
+        string constr = System.Configuration.ConfigurationManager.ConnectionStrings["myNewDatabase"].ConnectionString;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        //excel connection
+        private void ExcelConn(string constring)
+        {
+            //connection = string.Format(@"Provider=Microsoft.ACE.OLEDB.8.0;Data Source={'"+ Path +"'};Extended Properties='Excel 8.0 Xml; HDR = No;'");
+            con = new OleDbConnection(constring);
+        }
+
+        protected void btnUpload_Click(object sender, EventArgs e)
+        {
+            //checking if the file has been uploaded
+            if (uploadFile.HasFile)
+            {
+                //try catch to handle exceptions
+                try
+                {
+                    string path;
+                    //creating upload path
+                    path = Server.MapPath("~/Uploads/");
+                    if (!Directory.Exists(path))
+                    {
+                        //creating a directory
+                        Directory.CreateDirectory(path);
+                    }
+                    //getting file name
+                    string filename = Path.GetFileName(uploadFile.FileName);
+                    //replacing file with new name
+                    //string newfile = filename.Replace(filename, "UpdatedEmandatefile.xls");
+                    //saving file in uploads folder
+                    uploadFile.SaveAs(Server.MapPath("~/Uploads/") + filename);
+                    path = Server.MapPath("~/Uploads/" + filename);
+                    //getting file extension
+                    string extension = Path.GetExtension(uploadFile.PostedFile.FileName);
+                    //emptying the connection string container
+                    constring = string.Empty;
+
+                    switch (extension)
+                    {
+                        case ".xls": // Excel 97-03
+                            constring = $"Provider=Microsoft.Jet.OLEDB.4.0;Data Source={path};Extended Properties='Excel 8.0;HDR=YES';";
+                            proceed();
+                            break;
+                        case ".xlsx": // Excel 07 or higher only
+                            constring = $"Provider=Microsoft.ACE.OLEDB.12.0;Data Source={path};Extended Properties='Excel 12.0 Xml;HDR=YES'";
+                            proceed();
+                            break;
+                        default:
+                            // This block executes if extension does not match .xls or .xlsx
+                            ClientScript.RegisterStartupScript(GetType(), "alert", "alert('File must be in Xls or Xlsx format only!');", true);
+                            break;
+                    }
+
+                }
+                catch(Exception ex)
+                {
+                    //alert if failed to insert
+                    ClientScript.RegisterStartupScript(GetType(), "alert", "alert('" + ex.Message.ToString() + "');", true);
+                }
+            }
+        }
+
+        protected void proceed()
+        {
+            //excel connection 
+            ExcelConn(constring);
+            //reads excel data
+            DataTable dt2 = ReadExcelRecords();
+            //inserting excel data
+            insertDataIntoDatabase(dt2);
+            //alert if sucessfully inserted
+            ClientScript.RegisterStartupScript(GetType(), "alert", "alert('Excel Record entered into database Sucessfully!');", true);
+        }
+
+        //reads excel data method
+        protected DataTable ReadExcelRecords()
+        {
+            //try catch to handle exceptions
+            try
+            {
+                //opening connection
+                con.Open();
+                //selecting a sheet in excel
+                string sheet1 = con.GetOleDbSchemaTable(OleDbSchemaGuid.Tables, null).Rows[0]["TABLE_NAME"].ToString();
+                //creating a new datatable
+                DataTable dt = new DataTable();
+                //creating columns for data table
+                dt.Columns.AddRange(new DataColumn[5] {
+                    new DataColumn("userid", typeof(int)),
+                    new DataColumn("Name", typeof(string)),
+                    new DataColumn("email", typeof(string)),
+                    new DataColumn("date", typeof(DateTime)),
+                    new DataColumn("phone", typeof(string))
+                });
+
+                //retriving data from excel
+                OleDbDataAdapter da = new OleDbDataAdapter("SELECT * FROM [" + sheet1 + "]", con);
+
+                //filling table into data table
+                da.Fill(dt);
+                //ittrating each row
+                foreach (DataRow row in dt.Rows)
+                {
+                    // getting date&time
+                    DateTime orgDateTime = Convert.ToDateTime(row["date"]);
+
+                    // format date and time 
+                    string formatedDate = orgDateTime.ToString("yyyy-MM-dd hh:mm:ss tt");
+
+                    // Assign the formatted date string back to the "date" column in the DataRow
+                    row["date"] = formatedDate;
+                }
+
+                //returning table
+                return dt;
+
+            }
+            //exception
+            catch(Exception ex)
+            {
+                //alert 
+                ClientScript.RegisterStartupScript(GetType(), "alert", "alert('" + ex.Message.ToString() + "');", true);
+                return null;
+            }
+        }
+
+        protected void insertDataIntoDatabase(DataTable dt)
+        {
+            try
+            {
+                //creating connection
+                SqlConnection con = new SqlConnection(constr);
+                //bulk upload creating
+                SqlBulkCopy bc = new SqlBulkCopy(con);
+                //bul upload table calling  
+                bc.DestinationTableName = "dbo.DataEntry";
+                //bulk upload column maping
+                bc.ColumnMappings.Add("userid", "userid");
+                bc.ColumnMappings.Add("name", "name");
+                bc.ColumnMappings.Add("email", "email");
+                bc.ColumnMappings.Add("date", "date");
+                bc.ColumnMappings.Add("phone", "phone");
+                //opening sql connection
+                con.Open();
+                //writing to server
+                bc.WriteToServer(dt);
+                //closing sql connection
+                con.Close();
+            }
+            catch(Exception ex)
+            {
+                //alert 
+                ClientScript.RegisterStartupScript(GetType(), "alert", "alert('" + ex.Message.ToString() + "');", true);
+            }
+        }
+
+        protected void btnView_Click(object sender, EventArgs e)
+        {
+            try {
+                string qry = "SELECT * FROM DATAENTRY";
+                SqlConnection con = new SqlConnection(constr);
+                con.Open();
+                SqlCommand cmd = new SqlCommand(qry, con);
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                DataTable dt = new DataTable();
+                
+                da.Fill(dt);
+                con.Close();
+
+                repeaterView.DataSource = dt;
+                repeaterView.DataBind();
+            }
+            catch(Exception ex)
+            {
+                //alert 
+                ClientScript.RegisterStartupScript(GetType(), "alert", "alert('" + ex.Message.ToString() + "');", true);
+            }
+        }
+    }
+}
